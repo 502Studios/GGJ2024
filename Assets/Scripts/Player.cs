286b27d6d3aaca3e24600d9997befbfd321dfda0
Bryan Alvarado <bryan@502Studios.net>
2024-01-27 14:46:27 -0600

    Created multiplayer player manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        Debug.Log($"Player index is {playerInput.playerIndex}");
+    }
 }
