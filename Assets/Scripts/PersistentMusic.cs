ffd22de91094e81cf7300bc4534521be916845cf
Bryan Alvarado <bryan@502Studios.net>
2024-01-28 13:25:49 -0600

    Fixed the persistent music script

--- Assets/Scripts/PersistentMusic.cs
+++ Assets/Scripts/PersistentMusic.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class PersistentMusic : MonoBehaviour
 {
+
+    public static PersistentMusic instance;
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
